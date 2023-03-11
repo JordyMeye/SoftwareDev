86e85e95e9959041764cc8a47a9784a122389d55
Jordy Meye <220072841@mycput.ac.za>
2023-03-11 22:18:57 +0200

    this is the second commit: code being added into the customer pojo class and testing class

--- src/main/java/za/ac/cput/domain/Customer.java
+++ src/main/java/za/ac/cput/domain/Customer.java
@@ -2,7 +2,48 @@ package za.ac.cput.domain;
 
 public class Customer {
 
+private String Name;
+private String Surname;
+private int Age ;
 
 
 
+    public Customer(String name, String surname, int age) {
+        Name = name;
+        Surname = surname;
+        Age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "Name='" + Name + '\'' +
+                ", Surname='" + Surname + '\'' +
+                ", Age=" + Age +
+                '}';
+    }
 }
--- src/main/java/za/ac/cput/test/CustomerTest.java
+++ src/main/java/za/ac/cput/test/CustomerTest.java
@@ -1,4 +1,36 @@
 package za.ac.cput.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import za.ac.cput.domain.Customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class CustomerTest {
+
+
+    private Customer customer1;
+    private Customer customer2;
+    private Customer customer3;
+
+
+    public void setUp() {
+        customer1 = new Customer("John", "Doe", 29);
+        customer2 = new Customer("John", "Doe", 29);
+        customer3 = new Customer("Jane", "Tim", 26);
+
+
+    }
+
+
+
+
+    @Test
+    public void testObjectEquality() {
+        assertEquals(customer1, customer2);
+
+        assertNotEquals(customer3, customer1);
+
+    }
 }
