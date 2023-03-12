2ebe54837ac0c0cfd63bc208a1b022e7e0704654
siyathandaza <smqhamane98@gmail.com>
2023-03-12 19:23:18 +0200

    Calculator pojo class added CalculatorTest class added

---
+++ src/main/java/za/ac/cput/domain/Calculator.java
@@ -0,0 +1,22 @@
+package za.ac.cput.domain;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
